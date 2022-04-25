a48419bbd9d1f159ecd216f2734e8068bd4792a9
dada-tech <david.olu.dada@gmail.com>
2022-04-25 01:10:46 -0600

    code cleanup for readability

--- tensor-transfer-model.js
+++ tensor-transfer-model.js
@@ -1,21 +1,57 @@
-let recognizer;
+// tf imported from script CDN
+// speechCommands imported from script CDN
 
-function predictWord() {
-  // Array of words that the recognizer is trained to recognize.
-  const words = recognizer.wordLabels();
-  recognizer.listen(({scores}) => {
-    // Turn scores into a list of (score,word) pairs.
-    scores = Array.from(scores).map((s, i) => ({score: s, word: words[i]}));
-    // Find the most probable word.
-    scores.sort((s1, s2) => s2.score - s1.score);
-    document.querySelector('#output-tensor').textContent = scores[0].word;
-  }, {probabilityThreshold: 0.75});
+let baseRecognizer;
+let words = []; // Array of words that the recognizer is trained to recognize.
+
+// baseRecognizer.listen config can adjust fields such as
+//    - includeSpectrogram
+//    - probabilityThreshold
+//    - includeEmbedding
+const recognitionConfig = {
+  probabilityThreshold: 0.75
 }
 
-async function app() {
-  recognizer = speechCommands.create('BROWSER_FFT');
-  await recognizer.ensureModelLoaded();
-  predictWord();
+const predictWordStart = () => {
+  // `listen()` takes two arguments:
+// 1. A callback function that is invoked anytime a word is recognized.
+// 2. A configuration object with adjustable fields
+  baseRecognizer.listen(onWordRecognize, recognitionConfig);
 }
 
-export { predictWord, app }
+const predictWordStop = (seconds = 0) => {
+  setTimeout(() => {
+    baseRecognizer.stopListening()
+    console.log('listening ended');
+  }, seconds * 1000);
+}
+
+//  result.scores contains the probability scores that correspond to recognizer.wordLabels().
+//  result.spectrogram contains the spectrogram of the recognized word.
+const onWordRecognize = (result) => {
+  let {scores} = result;
+  // Turn scores into a list of (score,word) pairs.
+  scores = Array.from(scores).map((s, i) => ({score: s, word: words[i]}));
+  // Find the most probable word.
+  scores.sort((s1, s2) => s2.score - s1.score);
+  document.querySelector('#output-tensor').textContent = scores[0].word;
+}
+
+export async function app() {
+  // When calling `create()`, you must provide the type of the audio input.
+  // The two available options are `BROWSER_FFT` and `SOFT_FFT`.
+  // - BROWSER_FFT uses the browser's native Fourier transform.
+  // - SOFT_FFT uses JavaScript implementations of Fourier transform
+  //   (not implemented yet).
+  baseRecognizer = speechCommands.create('BROWSER_FFT');
+
+  // Make sure that the underlying model and metadata are loaded via HTTPS
+  // requests.
+  await baseRecognizer.ensureModelLoaded();
+
+  // array of words that the recognizer is trained to recognize.
+  words = baseRecognizer.wordLabels();
+
+  predictWordStart();
+  predictWordStop(100);
+}
